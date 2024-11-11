28c3d09c034b4559bce06b97335a170ea9736bde
md-ahmodullah <contactme.ahmod@gmail.com>
2024-11-12 04:28:58 +0600

    Added Template Design

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,25 @@
 export default function About() {
   return (
     <>
-      <h1>About</h1>
+      <div className="text-white hero bg-base-200 my-12">
+        <section className="w-10/12 mx-auto my-12 min-h-[400px] flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center gap-5">
+            <h1 className="text-2xl md:text-4xl font-extrabold text-gray-400 text-center">
+              About Us
+            </h1>
+            <h1 className="text-2xl md:text-4xl font-extrabold text-gray-400 text-center">
+              Know About Us What We Do!
+            </h1>
+            <img
+              src="https://i.ibb.co.com/mCSw0t0/image.png"
+              alt="Dashboard Page"
+            />
+            <h2 className="text-2xl font-bold text-gray-500 text-center">
+              Read Carefully!
+            </h2>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,25 @@
 export default function Contact() {
   return (
     <>
-      <h1>Contact</h1>
+      <div className="text-white hero bg-base-200 my-12">
+        <section className="w-10/12 mx-auto my-12 min-h-[400px] flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center gap-5">
+            <h1 className="text-2xl md:text-4xl font-extrabold text-gray-400 text-center">
+              Contact Us
+            </h1>
+            <h1 className="text-2xl md:text-4xl font-extrabold text-gray-400 text-center">
+              You are a Free User!
+            </h1>
+            <img
+              src="https://i.ibb.co.com/Gk197M9/image.png"
+              alt="Dashboard Page"
+            />
+            <h2 className="text-2xl font-bold text-gray-500 text-center">
+              Contact Us Freely.
+            </h2>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
